c82269de7dbdedcabfd6f617385a0196db26c5f7
LUCIAN-DAVID ROȘU <lucian.rosu@stud.ubbcluj.ro>
2024-12-18 01:54:25 +0200

    Add project files.

---
+++ Rosu_Lucian_Lab11/Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rosu_Lucian_Lab11.Models;
+
+namespace WebAPI.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rosu_Lucian_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
---
+++ Rosu_Lucian_Lab11/Models/ShopList.cs
@@ -0,0 +1,10 @@
+﻿namespace Rosu_Lucian_Lab11.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
